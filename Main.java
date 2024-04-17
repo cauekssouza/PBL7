6016269a643e7dc63facb500c3cf5d6c9510019f
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-04-17 15:40:15 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Ebook ebook = new Ebook("C++", "Stephen R. Davis", 2016);
         System.out.println(ebook.descricao());
-        ebook.baixando();
+        ebook.baixar();
         System.out.println("-----------------------------\n");
         Video video = new Video("Java-Guia do Programador", "Peter JANDL JUNIOR", 2010);
         System.out.println(video.descricao());
-        video.baixando();
-        video.visualizando();
+        video.baixar();
+        video.visualizar();
         System.out.println("----------------------------\n");
     }
 }
