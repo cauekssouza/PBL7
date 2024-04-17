df4feea1476f6c0107fadb9c3c4eacbf76d65667
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-04-17 15:39:33 -0300

    Video.java

--- Video.java
+++ Video.java
@@ -7,11 +7,11 @@ public class Video extends ItemBibliotecaDigital implements Baixavel, Visualizar
         return "Vídeo Digital - Título: " + titulo + ", Autor: " + autor + ", Ano de Publicação: " + anoPublicacao; 
     }
     @Override
-    public void baixando(){
+    public void baixar(){
         System.out.println("Baixando o Vídeo Digital: " + titulo);
     }
     @Override
-    public void visualizando(){
+    public void visualizar(){
         System.out.println("Visualizando o vídeo digital: " + titulo);
     }
 }
